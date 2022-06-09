8ebb318e5290e6c2963e6ec1d896d7d5b0b9849e
Mary Martinez <mary.diana.martinez@gmail.com>
2022-06-09 11:47:27 -0700

    list is rendering and test is passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -12,7 +12,7 @@ describe('backend-express-template routes', () => {
   });
   it('/signs should return a list of signs', async () => {
     const res = await request(app).get('/signs');
-    const expected = [];
+    const expected = [{ word: 'More', introduced: true }, { word: 'Finish', introduced: true }, { word: 'Milk', introduced: true }, { word: 'Pain', introduced: true }, { word: 'Potty', introduced: true }, { word: 'Eat', introduced: false }, { word: 'Thank you', introduced: false }, { word: 'Sleep', introduced: false }, { word: 'I love you', introduced: true }];
     expect(res.body).toEqual(expected);
   });
   afterAll(() => {
--- sql/setup.sql
+++ sql/setup.sql
@@ -10,15 +10,15 @@ CREATE table signs (
   url VARCHAR NOT NULL,
   introduced BOOLEAN,
   proficiency INT NOT NULL
-)
+);
 
 INSERT INTO signs (word, aka, url, introduced, proficiency) VALUES
-('More', '', 'https://www.youtube.com/watch?v=nJwr-yWRdNQ', true, 10),
+('More', NULL, 'https://www.youtube.com/watch?v=nJwr-yWRdNQ', true, 10),
 ('Finish', 'All done', 'https://www.youtube.com/watch?v=v3ncKPr_8ro', true, 10),
-('Milk', '', 'https://www.youtube.com/watch?v=quk_XoRtZWk', true, 8),
-('Pain', '', 'https://www.youtube.com/watch?v=LHYJVE-Xb00', true, 9),
-('Potty', '', 'https://www.youtube.com/watch?v=bjYh6Ef1OYw', true, 3),
+('Milk', NULL, 'https://www.youtube.com/watch?v=quk_XoRtZWk', true, 8),
+('Pain', NULL, 'https://www.youtube.com/watch?v=LHYJVE-Xb00', true, 9),
+('Potty', NULL, 'https://www.youtube.com/watch?v=bjYh6Ef1OYw', true, 3),
 ('Eat', 'Food', 'https://www.youtube.com/watch?v=2Dl1Q4vlQKs', false, 0),
 ('Thank you', 'Thanks', 'https://www.youtube.com/watch?v=2W0BDFUjsG0', false, 0),
 ('Sleep', 'Nap', 'https://www.youtube.com/watch?v=dXEqaWSEztY', false, 0),
-('I love you', '', 'https://www.youtube.com/watch?v=OMOatZwMRak', true, 2)
+('I love you', NULL, 'https://www.youtube.com/watch?v=OMOatZwMRak', true, 2)
